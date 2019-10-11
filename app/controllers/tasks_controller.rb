f9bfe515eede70593d74ad11c8fba0815d85959a
EC2 Default User <ec2-user@ip-172-31-35-170.ap-northeast-1.compute.internal>
2019-10-11 15:03:32 +0000

    task_app_C7_C8_基本的な機能は終了_ボタン類などまだ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.where(user_id:params[:user_id])
     @user = User.find_by(id: params[:user_id])
@@ -21,10 +23,39 @@ class TasksController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    @task.user_id= params[:user_id]
+    if @task.update_attributes(task_params)
+      flash[:success] = "タスク情報を更新しました。"
+      redirect_to user_tasks_path(params[:user_id])
+    else
+      render :edit
+    end
+  end
+
+  # attendance system の　user update
+  #def update
+  #  if @user.update_attributes(user_params)
+  #    flash[:success] = "ユーザー情報を更新しました。"
+  #    redirect_to @user
+  #  else
+  #    render :edit      
+  #  end
+  #end
+
+  def show
+  end
+  
   private
 
     def task_params
       params.require(:task).permit(:title, :details, :user_id)
     end
-    
+
+    def set_task
+      @task = Task.find_by(id:params[:id], user_id:params[:user_id])
+    end
 end
