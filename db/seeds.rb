dce15fb4bc511744402d417d268bba44e4a3884b
EC2 Default User <ec2-user@ip-172-31-35-170.ap-northeast-1.compute.internal>
2019-10-15 14:39:30 +0000

    task_app_B5_B6_終了

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,7 @@ User.create!( name: "Sample User",
               email: "sample@email.com",
               password: "password",
               password_confirmation: "password")
+User.create!( name: "Sample User2",
+              email: "sample-2@email.com",
+              password: "password",
+              password_confirmation: "password")
