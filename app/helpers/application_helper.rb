02382eb129269385ce68a2e645790d3752003c07
EC2 Default User <ec2-user@ip-172-31-35-170.ap-northeast-1.compute.internal>
2019-10-31 13:01:59 +0000

    task_app_G23_終了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  require "uri"
 
   def full_title(page_name = "")
     base_title = "Task App"
@@ -8,4 +9,12 @@ module ApplicationHelper
       page_name + " | " + base_title
     end
   end
+  
+  def text_url_to_link(text)
+    URI.extract(text, ["http","https"]).uniq.each do |url|
+      text.gsub!(url, "<a href=\"#{url}\" target=\"_blank\">#{url}</a>")
+    end
+    text
+  end
+  
 end
